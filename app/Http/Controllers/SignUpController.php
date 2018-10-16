d01759c18b43303c0416a5bff89bf3e1dee3f6c6
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2018-10-16 20:39:21 +0800

    correcting signUp function

---
+++ app/Http/Controllers/SignUpController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+
+class SignUpController extends Controller
+{
+    
+    public function design(Request $request){
+    	return view('signUp');
+    }
+
+
+    public function signUp(Request $request){
+
+    	$validate_rule=[
+
+    		'name'=>'required|between:2,20|unique:users',
+    		'email'=>'required|unique:users',
+    		'password'=>'required|between:5,10',
+    		'avatar'=>'required|image|mimes:jpeg,png,jpg,gif|max:3000',
+
+
+    	];
+    	$this->validate($request,$validate_rule);
+
+    	$param=[
+
+    		'name'=>$request->name,
+    		'email'=>$request->email,
+    		'password'=>Hash::make($request->password),
+    		'avatar'=>$request->avatar,
+    	];
+
+    	DB::table('users')->insert($param);
+
+    	$image=$request->file('avatar');
+    	$new_name=rand() . '.' . $image->getClientOriginalExtension();
+    	$image->move(public_path("images"),$new_name);
+
+    	return view('signUp',['name'=>$request->name,'email'=>$request->email,'password'=>Hash::make($request->password),'avatar'=>$request->avatar,redirect()->back()->withSuccess('Registrarion successfully!')]);
+    }   	
+}
---
+++ resources/views/signUp.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>SignUp Page</title>
+    <link rel="stylesheet" href="../css/signUp.css">
+</head>
+<body>
+<h1>SignUp Page</h1>
+<div class="signUp">
+<form action="/signUp" method="POST" enctype="multipart/form-data">
+{{csrf_field()}}
+	<div class="form-group">
+	<span>Name:</span>
+	<input type="text" name="name" value="{{old('name')}}"><br>
+	@if($errors->has('name'))
+	<em>{{$errors->first('name')}}</em>
+	@endif
+	</div>
+	<div class="form-group">
+	<span>Avatar:</span>
+	<input type="file" name="avatar"><br>
+	@if($errors->has('avatar'))
+	<em>{{$errors->first('avatar')}}</em>
+	@endif
+	</div>
+	<div class="form-group">
+	<span>Email:</span>
+	<input type="email" name="email" value="{{old('email')}}"><br>
+	@if($errors->has('email'))
+	<em>{{$errors->first('email')}}</em>
+	@endif
+	</div>
+	<div class="form-group">
+	<span>Password:</span>
+	<input type="password" name="password" value="{{old('password')}}"><br>
+	@if($errors->has('password'))
+	<em>{{$errors->first('password')}}</em>
+	@endif
+	</div>
+	<div class="form-button">
+	<input type="reset" value="reset">
+	<input type="submit" value="Sign Up">
+	</div>
+	<div class="success">
+	@if(session('success'))
+		<em>{{session('success')}}</em>
+	@endif
+	</div>
+	</form>
+</div>
+</body>
+</html>
