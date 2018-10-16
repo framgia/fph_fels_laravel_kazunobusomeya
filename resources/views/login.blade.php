8ba888687b768795300fb270853c6185e4b8c0f6
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2018-10-16 20:39:21 +0800

    making login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>Login Page</title>
+</head>
+<style>
+body{width:600px; margin-left:auto; margin-right:auto; height:500px; background-color:skyblue;}
+h1{text-align:center; font-size:20px;}
+.login{border:1px solid black; height:300px; background-color:white;}
+span{font-weight:bold;}
+
+</style>
+<body>
+<h1>Framgia E-learning System</h1>
+<div class="login">
+<span>User name:</span>
+<input type="text" name="name"><br>
+<span>Password:</span>
+<input type="password" name="password"><br>
+<input type="submit" name="login" value="login">
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,39 +9,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+*/
 
 
-Route::get('hello', function () {
-    return view('welcome');
-});*/
 
-$html = <<< EOF
-<html>
-<head>
-<title>Login</title>
-<style>
-body{width:400px; margin-left:auto; margin-right:auto; background-color:skyblue;}
-h1{font-weight:bold; font-size:20px; text-align:center;}
-.login{background-color:white; height:500px; width:100%;}
-
-</style>
-</head>
-<body>
-	<h1>Framgia E-learning System</h1>
-	<div class="login">
-	<span>Username</span>
-	<input type="text" name="name"><br>
-	<span>Password</span>
-	<input type="password" name="password">
-	</div>
-</body>
-</html>
-EOF;
-
-Route::get('/',function() use ($html){
-	return $html;
+Route::get('login',function(){
+	return view('login');
 
 });
-
-
 ?>
